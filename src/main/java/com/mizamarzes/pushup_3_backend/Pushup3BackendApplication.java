53b9fbd6c3b80d4409bf3fc3ce6d8f36637031c9
mizamarzes <mizamarzes@gmail.com>
2024-09-19 14:51:21 -0500

    first commit

---
+++ src/main/java/com/mizamarzes/pushup_3_backend/Pushup3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.mizamarzes.pushup_3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pushup3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pushup3BackendApplication.class, args);
+	}
+
+}
